fc6d418ca1a8085487a76bba8ae36e6c19ce4d6a
adiffloth <adiffloth@gmail.com>
2022-12-07 11:54:53 -0800

    Day 7

---
+++ day07/part_1.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+lines = [x for x in open('day07/1.in').read().splitlines()]
+
+pwd = ['/']
+files = defaultdict(int)
+for line in lines[1:]:
+    if line == '$ cd ..':
+        pwd.pop()
+    elif line.startswith('$ cd'):
+        pwd.append(line.split()[2] + '/')
+    elif line[0].isnumeric():
+        size, file_name = line.split()
+        files[''.join(pwd + [file_name])] = int(size)
+
+dirs = defaultdict(int)
+for path, size in files.items():
+    path_ls = path.split('/')[:-1]
+    while path_ls:
+        dirs['/'.join(path_ls)] += size
+        path_ls.pop()
+
+part_1 = sum([x for x in dirs.values() if x <= 100_000])
+print(f'Part 1: {part_1}')
+assert part_1 == 1490523
+
+space_needed = 30000000 - (70000000 - dirs[''])
+part_2 = min([x for x in dirs.values() if x >= space_needed])
+print(f'Part 2: {part_2}')
+assert part_2 == 12390492
+
+print('Tests passed.')
