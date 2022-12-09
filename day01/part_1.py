62cc7b84164c826586fde0a2c56f86c4a89d5144
adiffloth <adiffloth@gmail.com>
2022-12-08 18:21:09 -0800

    Day 1 clean up

--- day01/part_1.py
+++ day01/part_1.py
@@ -9,14 +9,5 @@ for line in open('day01/1.in').read().splitlines():
         cals = 0
 
 elves.append(cals)  # Don't forget the last one
-elves.sort(reverse=True)
 
-part_1 = elves[0]
-part_2 = sum(elves[:3])
-
-print(f'{part_1 = }')
-print(f'{part_2 = }')
-
-assert part_1 == 69626
-assert part_2 == 206780
-print('Tests passed.')
+print(f'{max(elves)}')
